56ccbc9c0673e460ee45aff27d1b8e7011f0069f
Admir <xomodo@gmail.com>
2024-01-20 17:24:01 +0100

    Update HelloService

--- src/Services/HelloService.cs
+++ src/Services/HelloService.cs
@@ -3,4 +3,12 @@ namespace app.Services;
 public class HelloService
 {
     public string GetMessage() => $"Hello from {nameof(HelloService)} Service!";
+
+    public async Task<string> GetUrlAsync(string url)
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", $"C# App ({nameof(HelloService)})");
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
