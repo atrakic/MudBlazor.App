2343039602473aadcb3a1c3c8451f7658837e610
atrakic <atrakic@users.noreply.github.com>
2024-05-31 08:06:30 +0200

    Update

--- .github/actions/deploy.sh
+++ .github/actions/deploy.sh
@@ -3,7 +3,7 @@
 set -exo pipefail
 
 ## Prepare
-docker compose -f ./init.yml -f ./production.yml pull --ignore-buildable
+# docker compose -f ./init.yml -f ./production.yml pull --ignore-buildable
 
 ## Deploy
 docker compose -f ./init.yml -f ./production.yml up --pull always --remove-orphans --no-color -d
@@ -14,6 +14,7 @@ docker exec nginx-proxy-acme /app/cert_status
 docker exec nginx-proxy-acme cat /etc/nginx/conf.d/default.conf
 
 docker compose -f init.yml -f production.yml top
+docker compose -f init.yml -f production.yml stats --no-stream
 
 ## Clean up
 docker system prune -f
