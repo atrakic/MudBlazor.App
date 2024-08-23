a4c25fda99c8ee6c1157457fdae82e0a6c65eafc
atrakic <atrakic@users.noreply.github.com>
2024-08-23 14:15:24 +0200

    [ci-skip] Add tests

---
+++ tests/e2e/products.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.use({ baseURL: process.env.STORE_ADMIN_URL });
+
+test('renders functionally', async ({ page }) => {
+    await page.goto('/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Products' }).click();
+
+    // Expects page to have an Add Product button.
+    await expect(page.getByRole('button', { name: 'Add Product' })).toBeVisible();
+    await expect(page.getByText('Product ID')).toBeVisible();
+    await expect(page.getByText('Product Name')).toBeVisible();
+    await expect(page.getByText('Product Description')).toBeVisible();
+  });
